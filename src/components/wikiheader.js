758c22f0ccaf71850c04082d34f166e69979a88e
Jimmie <jtarrodean@gmail.com>
2019-06-03 16:33:38 -0700

    additional css fixes

--- src/components/wikiheader.js
+++ src/components/wikiheader.js
@@ -4,7 +4,7 @@ const WikiHeader = () => {
     return(
         <header>
             <h1 className="header"><span><i className="fab fa-wikipedia-w"></i></span>iki<span id="title">Viewer</span></h1>
-            <h4 className="mini-heading">Getting lost "link-surfing" on Wikipedia has never been so easy surfs up...</h4>
+            <h4 className="mini-heading">Wikipedia article search and link surfing tool</h4>
         </header>
     );
 }
